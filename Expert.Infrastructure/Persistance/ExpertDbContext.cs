20d8285fc965639ebf231feea33f6139e51745dd
flavio <flavi@DESKTOP-USTCPSC>
2024-05-10 14:36:10 -0300

    feat: Add entity core tools , sql server and add dbset for database

--- Expert.Infrastructure/Persistance/ExpertDbContext.cs
+++ Expert.Infrastructure/Persistance/ExpertDbContext.cs
@@ -1,31 +1,14 @@
 ﻿using Expert.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Expert.Infrastructure.Persistance
 {
-    public class ExpertDbContext
+    public class ExpertDbContext(DbContextOptions<ExpertDbContext> options) : DbContext(options)
     {
-        public ExpertDbContext()
-        {
-            Projects =
-            [
-                new Project("Meu Projeto 1", "Minha descricao  1", 1, 1, 10000),
-                new Project("Meu Projeto 2", "Minha descricao  2", 2, 2, 20000),
-            ];
-            Users =
-            [
-             new User("Flavio", "flaviojcostafilho@gmail.com", new DateTime(2000,04,04)),
-             new User("Flavio", "flaviojcostafilho@gmail.com", new DateTime(2000,04,04)),
-            ];
-            Skills =
-            [
-             new Skill(".net"),
-             new Skill("SQL"),
-            ];
-        }
-
-        public List<Project> Projects { get; set; }
-        public List<User> Users { get; set; }
-        public List<Skill> Skills { get; set; }
-        public List<ProjectComment> Comments { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<UserSkill> UserSkills { get; set; }
+        public DbSet<ProjectComment> Comments { get; set; }
     }
 }
