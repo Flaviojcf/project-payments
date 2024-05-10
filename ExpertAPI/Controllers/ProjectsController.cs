d0186b51cd365a55faf73f355f506ce11be5028d
flavio <flavi@DESKTOP-USTCPSC>
2024-05-09 21:51:28 -0300

    feat: Create Projects controller

--- ExpertAPI/Controllers/ProjectsController.cs
+++ ExpertAPI/Controllers/ProjectsController.cs
@@ -1,16 +1,87 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Expert.Application.InputModels;
+using Expert.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ExpertAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProjectsController : ControllerBase
+    public class ProjectsController(IProjectService projectService) : ControllerBase
     {
+        private readonly IProjectService _projectService = projectService;
+
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string query)
         {
-            return Ok();
+            var projects = _projectService.GetAll(query);
+
+            return Ok(projects);
         }
 
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var project = _projectService.GetById(id);
+
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(project);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectInputModel inputModel)
+        {
+            if (inputModel.Title.Length > 50) return BadRequest();
+
+            var id = _projectService.Create(inputModel);
+
+            return CreatedAtAction(nameof(GetById), new { id = id }, inputModel);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectInputModel inputModel)
+        {
+            if (inputModel.Title.Length > 200) return BadRequest();
+
+            _projectService.Update(inputModel);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _projectService.Delete(id);
+
+            return NoContent();
+        }
+
+
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment([FromBody] CreateCommentInputModel inputModel)
+        {
+            _projectService.CreateComment(inputModel);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            _projectService.Start(id);
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            _projectService.Finish(id);
+
+            return NoContent();
+        }
     }
 }
