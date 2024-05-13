0937fa1e15468d165bc521738b408e8169a6e8b6
flavio <flavi@DESKTOP-USTCPSC>
2024-05-13 07:58:56 -0300

    chore: Add mediatR

--- Expert.API/Program.cs
+++ Expert.API/Program.cs
@@ -1,6 +1,8 @@
+using Expert.Application.Commands.CreateProject;
 using Expert.Application.Services.Implementations;
 using Expert.Application.Services.Interfaces;
 using Expert.Infrastructure.Persistance;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +16,10 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ExpertDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ISkillService, SkillService>();
+
+builder.Services.AddMediatR(typeof(CreateProjectCommand));
 
 var app = builder.Build();
 
