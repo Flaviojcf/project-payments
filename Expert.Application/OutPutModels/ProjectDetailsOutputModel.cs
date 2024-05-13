609eb8d09cf2399b0626e7ead5c0f06365769b5c
flavio <flavi@DESKTOP-USTCPSC>
2024-05-13 07:27:47 -0300

    refactor: Add client and freelancer references

--- Expert.Application/OutPutModels/ProjectDetailsOutputModel.cs
+++ Expert.Application/OutPutModels/ProjectDetailsOutputModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace Expert.Application.OutPutModels
 {
-    public class ProjectDetailsOutputModel(int id, string title, string description, decimal totalCost, DateTime? startedAt, DateTime? finishedAt)
+    public class ProjectDetailsOutputModel(int id, string title, string description, decimal totalCost, DateTime? startedAt, DateTime? finishedAt, string clientFullName, string freelancerFullName)
     {
         public int Id { get; private set; } = id;
         public string Title { get; private set; } = title;
@@ -8,5 +8,7 @@
         public decimal TotalCost { get; private set; } = totalCost;
         public DateTime? StartedAt { get; private set; } = startedAt;
         public DateTime? FinishedAt { get; private set; } = finishedAt;
+        public string ClientFullName { get; private set; } = clientFullName;
+        public string FreelancerFullName { get; private set; } = freelancerFullName;
     }
 }
