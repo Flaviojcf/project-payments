e31c59c256c68a9195e15cec07a2072c26fb7db8
flavio <flavi@DESKTOP-USTCPSC>
2024-05-13 07:27:35 -0300

    feat: Create user input and output models

---
+++ Expert.Application/InputModels/CreateUserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Expert.Application.InputModels
+{
+    public class CreateUserModel(string fullName, string email, DateTime birthDate)
+    {
+        public string FullName { get; set; } = fullName;
+        public string Email { get; set; } = email;
+        public DateTime BirthDate { get; set; } = birthDate;
+    }
+}
---
+++ Expert.Application/OutPutModels/UserOutputModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Expert.Application.OutPutModels
+{
+    public class UserOutputModel(string fullName, string email)
+    {
+        public string FullName { get; set; } = fullName;
+        public string Email { get; set; } = email;
+    }
+}
