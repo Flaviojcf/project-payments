a2a38cb70541534fe12ee481f7529962574b920d
flavio <flavi@DESKTOP-USTCPSC>
2024-05-16 09:01:16 -0300

    feat: Add unit test project

---
+++ Expert.UnitTests/Domain/Entities/ProjectUnitTests.cs
@@ -0,0 +1,31 @@
+﻿using Expert.Domain.Entities;
+using Expert.Domain.Enums;
+
+namespace Expert.UnitTests.Domain.Entities
+{
+    public class ProjectUnitTests
+    {
+        [Fact]
+        public void TestIfProjectStartWorks()
+        {
+            var project = new Project("Nome", "Descrição", 10, 1, 2);
+
+            Assert.Equal(ProjecStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+
+            project.Start();
+
+            Assert.Equal(ProjecStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+
+    }
+}
