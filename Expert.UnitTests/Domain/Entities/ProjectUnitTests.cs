1fa4b9c5660e39042084bdc98902b64903afb45b
flavio <flavi@DESKTOP-USTCPSC>
2024-05-17 21:58:08 -0300

    refactor: Add AAA pattern

--- Expert.UnitTests/Domain/Entities/ProjectUnitTests.cs
+++ Expert.UnitTests/Domain/Entities/ProjectUnitTests.cs
@@ -32,21 +32,13 @@ namespace Expert.UnitTests.Domain.Entities
         [Trait("Domain", "Project - Aggregates")]
         public void TestIfProjectStartWorks()
         {
+            //Arrange
             var project = new Project("Nome", "Descrição", 10, 1, 2);
 
-            Assert.Equal(ProjecStatusEnum.Created, project.Status);
-            Assert.Null(project.StartedAt);
-
-            Assert.NotNull(project.Title);
-            Assert.NotEmpty(project.Title);
-
-
-            Assert.NotNull(project.Description);
-            Assert.NotEmpty(project.Description);
-
-
+            //Act
             project.Start();
 
+            //Assert
             Assert.Equal(ProjecStatusEnum.InProgress, project.Status);
             Assert.NotNull(project.StartedAt);
         }
